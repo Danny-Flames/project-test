e6047c299ba7c2fd2ddc3bdad6f5d19194e41962
Danny-Flames <danniskolobosiky@gmail.com>
2025-02-06 00:17:14 +0100

    added loading state for the table

--- src/loader/SkeletonLoaderTable.tsx
+++ src/loader/SkeletonLoaderTable.tsx
@@ -1,7 +1,7 @@
 const SkeletonLoaderTable = () => {
     return (
       <div className="space-y-2 animate-pulse">
-        {[...Array(5)].map((_, index) => (
+        {[...Array(7)].map((_, index) => (
           <div key={index} className="h-12 w-full bg-gray-200 rounded"></div>
         ))}
       </div>
